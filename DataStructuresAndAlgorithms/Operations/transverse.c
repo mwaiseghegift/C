e3fabb881f8269f873720bf8b2a8c02f31695178
Gateway47 <itsregalo047@gmail.com>
2021-01-26 15:01:28 +0300

    first

---
+++ DataStructuresAndAlgorithms/Operations/transverse.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+main(){
+    int ageArray[] = {18,20,17,30,25};
+    int item = 10, k=20, n=5;
+    int i=0, j=n;
+    printf("This is the list:\n");
+    for(i; i<j; i++){
+        printf("age[%d] = %d \n", i, ageArray[i]);
+    }
+}
