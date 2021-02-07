2bbc3dab82401f39553f28c3350cce71fb53eddf
Gateway47 <itsregalo047@gmail.com>
2021-02-07 22:00:57 +0300

    recursion

---
+++ recursion/recursion1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int possible_times(int password_length){
+    if(password_length<=1){
+        return 1;
+    }
+    return password_length*possible_times*(password_length-1);
+}
+
+int main(){
+    printf("Enter the length of your password\n");
+    scanf("%d", &password_length);
+
+    printf("\n\nThe possible length is %d", possible_times);
+    return 0;
+}
