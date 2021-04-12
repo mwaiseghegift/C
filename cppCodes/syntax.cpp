aba98db8da746d7e69bd4b2afe2d4e983d824adb
Gateway47 <55269787+itsregalo@users.noreply.github.com>
2021-04-12 22:38:03 +0300

    Delete cppCodes directory

--- cppCodes/syntax.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-//using namespace std means that we can use names for objects and variables from the standard library
-
-int main(){
-    cout << "Hello World";
-    return 0;
-}
