3e51fa6c7d02bf7666d6ec9f5edef04e18cc04cd
retech <itsregalo047@gmail.com>
2021-08-26 23:11:58 +0300

    done cases

--- structures/struct1.c
+++ structures/struct1.c
@@ -1,24 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//function to convert the sales to contants for switch statement
 
-    int categoryOutcome(sales){
-        if(sales < 1000){
-            return 0;
-        }
-        else if(sales >= 1000 && sales < 2000){
-            return 1;
-        }
-        else if(sales >= 2000 && sales < 3000){
-            return 2;
-        }
-        else if(sales >= 3000 && sales < 4000){
-            return 3;
-        }
-        else if(sales >= 4000 && sales < 5000){
-            return 4;
-        }
+int categoryOutcome(sales){
+    if(sales < 1000){
+        return 0;
     }
+    else if(sales >= 1000 && sales < 2000){
+        return 1;
+    }
+    else if(sales >= 2000 && sales < 3000){
+        return 2;
+    }
+    else if(sales >= 3000 && sales < 4000){
+        return 3;
+    }
+    else if(sales >= 4000 && sales < 5000){
+        return 4;
+    }
+}
    
 
 int main(){
