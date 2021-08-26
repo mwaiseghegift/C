0e420150990acec6839ac90f30dfe9dae8917e2f
retech <itsregalo047@gmail.com>
2021-08-26 23:08:18 +0300

    use condition first

--- structures/struct1.c
+++ structures/struct1.c
@@ -1,49 +1,41 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+    int categoryOutcome(sales){
+        if(sales < 1000){
+            return 0;
+        }
+        else if(sales >= 1000 && sales < 2000){
+            return 1;
+        }
+        else if(sales >= 2000 && sales < 3000){
+            return 2;
+        }
+        else if(sales >= 3000 && sales < 4000){
+            return 3;
+        }
+        else if(sales >= 4000 && sales < 5000){
+            return 4;
+        }
+    }
    
 
 int main(){
 
     int sales, commission;
 
-    bool first   (>5000);
-    bool second  >=4000<5000;
-    third = >=3000<4000;
-    fourth = >=2000<3000;
-    fifth = >=1000<2000;
-    sixth = <1000;
-
-
     printf("Enter the amout \n");
-
     scanf("%d", &sales);
+
+    int catOutcome = categoryOutcome(sales);
 
     switch(enum){
         case(>5000):
            commission = (2.5/100)*sales;
            break;
 
-        case (>=4000<5000):
-            commission= (2/100)*sales;
-            break;
-
-
-        case (>=3000<3999):
-            commission = (1.5/100)*sales;
-            break;
-
-        case (>=2000<3000):
-            commission = (1/100)*sales;
-            break;
-
-        case (>=1000<2000):
-            commission = (0.5/100)*sales;
-            break;
-
-        case(<1000):
-            commission = 0;
-            break;
+        
 
         default:
             printf("Error! Please check your input");
