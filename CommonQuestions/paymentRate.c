2879daa9d6c3301a25cc4205eeefe77e8dd8a18e
retech <itsregalo047@gmail.com>
2021-08-27 23:40:05 +0300

    work rate

--- CommonQuestions/paymentRate.c
+++ CommonQuestions/paymentRate.c
@@ -5,4 +5,11 @@ int main(){
 
     printf("Enter the Hours Worked :\n");
     scanf("%d", &hours);
+
+    if(hours<=35){
+        net = hours * 8;
+    }else if(hours>35<=(35+25)){
+        int hoursExceeded = hours-35;
+        net = (35*8)+hoursExceeded*(1.5*8);
+    }
 }
