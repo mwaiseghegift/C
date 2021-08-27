6e2f26c6c1e6a8b589dfe8fe76112f4cb602fec0
retech <itsregalo047@gmail.com>
2021-08-27 23:45:29 +0300

    complete question 2

--- CommonQuestions/paymentRate.c
+++ CommonQuestions/paymentRate.c
@@ -8,8 +8,18 @@ int main(){
 
     if(hours<=35){
         net = hours * 8;
+        printf("Net pay: %d", net);
     }else if(hours>35<=(35+25)){
         int hoursExceeded = hours-35;
         net = (35*8)+hoursExceeded*(1.5*8);
+        printf("Net pay: %d", net);
+    }else if(hours>(35+25)){
+        int first35 = hours*8;
+        int extra25 = hours*(1.5*8);
+        int after = (hours - (35+25))*(8.2);
+
+        net = first35+extra25+after;
+        printf("Net pay: %d", net);
     }
+    return 0;
 }
