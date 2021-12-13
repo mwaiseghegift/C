f1570be37d8ca26bacfe0302d275afa930a7cc1d
retech <itsregalo047@gmail.com>
2021-12-14 00:18:48 +0300

    tower of hanoi

--- DataStructuresAndAlgorithms/towerOfHanoi.cpp
+++ DataStructuresAndAlgorithms/towerOfHanoi.cpp
@@ -1,20 +1,26 @@
 #include <iostream>
 using namespace std;
 
-void move(int, int, int, int);
+void towerOfHanoi(int, int, int, int);
 
 int main(){
-    int diskNum;
+    int diskNum; //No of disks to be move
     cout<<"Enter the number of disks: ";
     cin >> diskNum;
-    move(diskNum, 1,3,2);
+    towerOfHanoi(diskNum, 1,3,2);
     return 0;
 }
 
-void move(int count, int num1, int num3, int num2){
+void towerOfHanoi(int count, int num1, int num3, int num2){
     if (count>0){
-        move(count-1, num1, num2, num3);
+        towerOfHanoi(count-1, num1, num2, num3);
         cout<<"Move disk "<<count<<" from "<< num1<<" to "<<num3<<endl;
-        move(count-1, num2, num3, num1);
+        towerOfHanoi(count-1, num2, num3, num1);
     }
 }
+
+/*
+    int num1 --> rod where disk is present
+    int num2 --> Rod where the disk is to be moved
+    int num2 --> Auxilliary
+*/
