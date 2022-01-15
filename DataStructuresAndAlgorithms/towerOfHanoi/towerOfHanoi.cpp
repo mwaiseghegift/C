869ce49173451c67fa7f469ca60a6d441cde3d49
retech <itsregalo047@gmail.com>
2022-01-15 19:48:59 +0300

    final tower of hanoi

--- DataStructuresAndAlgorithms/towerOfHanoi/towerOfHanoi.cpp
+++ DataStructuresAndAlgorithms/towerOfHanoi/towerOfHanoi.cpp
@@ -2,12 +2,17 @@
 using namespace std;
 
 void towerOfHanoi(int, int, int, int);
+int step_counter = 0;
 
 int main(){
     int diskNum; //No of disks to be move
     cout<<"Enter the number of disks: ";
     cin >> diskNum;
     towerOfHanoi(diskNum, 1,3,2);
+
+    //show number of steps taken
+    cout<<"\vIt took "<<step_counter<<" steps for the "
+                <<diskNum<<" disks to reach destination";
     return 0;
 }
 
@@ -15,6 +20,7 @@ void towerOfHanoi(int count, int disk1, int disk3, int disk2){
     if (count>0){
         towerOfHanoi(count-1, disk1, disk2, disk3);
         cout<<"Move disk "<<count<<" from "<< disk1<<" to "<<disk3<<endl;
+        step_counter+=1;
         towerOfHanoi(count-1, disk2, disk3, disk1);
     }
 }
