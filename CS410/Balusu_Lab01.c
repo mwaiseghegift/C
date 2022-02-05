f86218414b5edf06f87abc268f8f24afaf378a53
retech <itsregalo047@gmail.com>
2022-02-05 23:10:42 +0300

    fork func

---
+++ CS410/Balusu_Lab01.c
@@ -0,0 +1,50 @@
+
+/*
+    CS 410 Operating Systems
+    Balusu
+    Lab 01
+*/
+#include <stdio.h>
+
+int main(){
+    char continueOption;
+    int m, n, c, k;
+
+    m = 0b10101;
+    printf("the binary 10101 in decimal is: %i \n", m);
+
+    for (;;){
+        printf("Enter an integer in decimal number system\n");
+        scanf("%d", &n);
+        printf("%d in binary number system is: \n", n);
+
+        for (c = 31; c >=0; c-- )
+        {
+            k = n >> c;
+            if (k & 1)
+                printf("1");
+            else
+                printf("0");
+        }
+        printf("\n");
+
+        printf("Do you wish to continue? Y/N: ");
+        scanf(" %c", &continueOption);
+
+        if (continueOption == 'Y'|| continueOption == 'y')
+            continue;
+        else if (continueOption == 'N'|| continueOption == 'n')
+            break;
+
+        else {
+            printf("Enter a valid input");
+            break;
+        }
+
+    }
+
+    return 0;
+}
+
+
+
