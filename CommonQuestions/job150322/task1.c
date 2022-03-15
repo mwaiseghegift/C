4d6463bd30e10bdb33a0fe6f2e855a8a4c758728
retech <itsregalo047@gmail.com>
2022-03-15 22:55:10 +0300

    final tests

--- CommonQuestions/job150322/task1.c
+++ CommonQuestions/job150322/task1.c
@@ -28,20 +28,5 @@ int main() {
     printf("The Average rank of male empoyees: %d", average);
 
 
-    printf("//////////////////////PART B /////////////////////\n");
-
-    int NB_LAPTOP = 4;
-    float laptopPrice[4];
-    printf("Please enter the price of 4 laptops: ");
-    scanf("%f %f %f %f\n", &laptopPrice[0], &laptopPrice[1], &laptopPrice[2], &laptopPrice[3]);
-    printf("Part B is created by studentyyy xxx - Course CS159 - Spring 2022 - ID = 47305");
-
-    float discount = 19;
-
-    
-
-
-
-
     return 0;
     }
--- CommonQuestions/job150322/task2.c
+++ CommonQuestions/job150322/task2.c
@@ -3,17 +3,31 @@
 int main(){
     printf("//////////////////////PART B /////////////////////\n");
 
-    int NB_LAPTOP = 4;
+    int NB_LAPTOP = 10;
     float laptopPrice[NB_LAPTOP];
     printf("Please enter the price of 4 laptops: ");
 
-    //laptopPrice input without loop
-    scanf("%f %f %f %f\n", &laptopPrice[0], &laptopPrice[1], &laptopPrice[2], &laptopPrice[3] );
-    printf("Part B is created by studentyyy xxx - Course CS159 - Spring 2022 - ID = 47305");
+    //get laptopPrice inputs
+    scanf("%f %f %f %f %f %f %f %f %f %f", &laptopPrice[0], &laptopPrice[1], &laptopPrice[2], &laptopPrice[3], &laptopPrice[4], &laptopPrice[5], &laptopPrice[6], &laptopPrice[7], &laptopPrice[8], &laptopPrice[9] );
 
-    //discount price
-    float discount = 0.19;
+     printf("Part B is created by student Maha Ali - Course CS159 - Spring 2022 - ID = 60124");
+     
+    //discount price from ID 6+0+1+2+4
+    float discount = 0.13;
 
-    //calculate the discount price
+    printf("Item    \t\t Original Price\t\t Price with Discount\n");
+    printf("-----   \t\t---------------\t\t--------------------------\n");
+    printf("Laptop  1\t\t %.2f\t\t %.2f\n", laptopPrice[0], laptopPrice[0] - laptopPrice[0] * discount);
+    printf("Laptop  2\t\t %.2f\t\t %.2f\n", laptopPrice[1], laptopPrice[1] - laptopPrice[1] * discount);
+    printf("Laptop  3\t\t %.2f\t\t %.2f\n", laptopPrice[2], laptopPrice[2] - laptopPrice[2] * discount);
+    printf("Laptop  4\t\t %.2f\t\t %.2f\n", laptopPrice[3], laptopPrice[3] - laptopPrice[3] * discount);
+    printf("Laptop  5\t\t %.2f\t\t %.2f\n", laptopPrice[4], laptopPrice[4] - laptopPrice[4] * discount);
+    printf("Laptop  6\t\t %.2f\t\t %.2f\n", laptopPrice[5], laptopPrice[5] - laptopPrice[5] * discount);
+    printf("Laptop  7\t\t %.2f\t\t %.2f\n", laptopPrice[6], laptopPrice[6] - laptopPrice[6] * discount);
+    printf("Laptop  8\t\t %.2f\t\t %.2f\n", laptopPrice[7], laptopPrice[7] - laptopPrice[7] * discount);
+    printf("Laptop  9\t\t %.2f\t\t %.2f\n", laptopPrice[8], laptopPrice[8] - laptopPrice[8] * discount);
+    printf("Laptop 10\t\t %.2f\t\t %.2f\n", laptopPrice[9], laptopPrice[9] - laptopPrice[9] * discount);
+
+
 
 }
