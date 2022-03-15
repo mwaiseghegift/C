3cb85dd7232687a16561d37fb571a1d008f60384
retech <itsregalo047@gmail.com>
2022-03-15 23:25:50 +0300

    final tests

--- CommonQuestions/job150322/task1.c
+++ CommonQuestions/job150322/task1.c
@@ -4,28 +4,27 @@ int main() {
     printf("//////////////////////PART A /////////////////////\n");
 
     int NB = 6;
-    int rank, average; 
+    float rank, average, sum=0; 
     char gender;
-    int sum=0, count=0;
+    int count=0;
 
     printf("Enter the rank and the gender (M or F) of %d employees\n", NB);
 
     // while loop to enter data into an array
     
     while (NB>0){
-        scanf("%d %c", &rank, &gender );
+        scanf("%f %c", &rank, &gender );
         if (gender == 'M'){
             sum = sum + rank;
             count = count+1;
         }
         NB=NB-1;
-        printf("%d\n", NB);
     }
 
-    printf("Part A is created by student yyy xxx- section F6 - ID =47305");
+    printf("Part A is created by student Maha Ali - Course CS159 - Spring 2022 - ID = 60124");
 
     average = sum/count;
-    printf("The Average rank of male empoyees: %d", average);
+    printf("The Average rank of male empoyees: %.1f", average);
 
 
     return 0;
--- CommonQuestions/job150322/task3.c
+++ CommonQuestions/job150322/task3.c
@@ -3,7 +3,7 @@
 int main(){
     printf("//////////////////////PART C /////////////////////\n");
 
-    int number = 47305;
+    int number = 60124;
     int numberArray[6];
     int reversedNumberArray[6];
     int k = 0,l = 0, j;
@@ -16,32 +16,30 @@ int main(){
     }
     printf("\n");
 
-    //numberArray reverse
+    //reversed number array
     for (j = k - 1; j > -1; j--) {
         reversedNumberArray[l] = numberArray[j];
         l++;
     }
 
-    //length of an newNumberArray
+    //length of an reversedNumberArray
     int arrayLength = sizeof(reversedNumberArray)/sizeof(reversedNumberArray[0]);
 
     //pattern of the number according to the length of the number in reversedNumberArray
     for (int i = 0; i < arrayLength; i++) {
         for (int m = 0; m < reversedNumberArray[i]; m++) {
-            printf("%d ", 47305);
+            printf("%d ", 60124);
         }
         printf("\n");
     }
 
-    /* 
-    Initializes two integers A and B with the first and second digits of your AUM ID, respectively. 
-    If your ID is 47305, the value of A will be 4 and the value of B will be 7
-    */
+
+   // Initialization of A and B 
     int A = reversedNumberArray[0];
     int B = reversedNumberArray[1];
     int M;
 
-    //if the value of A is 4 and the value of B is 7, the value of M will be 7
+    //Determining which value is greater than the other
     if (A > B) {
         M = A;
     }
@@ -50,40 +48,43 @@ int main(){
     }
     else  printf("Error");
 
-    printf("The value of M is %d", M);
+    printf("The value of M is %d\n", M);
 
     //Using a while loop, prompt the user to enter the price of M number of chairs
-    int NB_CHAIR = M;
+    int noOfChairs = M;
     float chairPrice[M];
 
     // while loop to enter prices of M number of chairs
 
-    int z=0, chairDiscount;
+    int z=0;
+    float chairDiscount, afterDiscount, totalPrice=0;
+
     while (z<M){
-        printf("Enter the price of the chair #%d", z);
+        printf("Enter the price of the chair #%d: ", z+1);
         scanf("%f", &chairPrice[z]);
 
         if (chairPrice[z]<1000){
             chairDiscount = 0.1;
-            printf("Price of chair #%d after discount is %f", chairPrice[z]*chairDiscount);
+            afterDiscount = chairPrice[z]-(chairPrice[z]*chairDiscount);
+            printf("Price of chair #%d after discount is %.3f\n", z+1, afterDiscount);
         }
         else if (chairPrice[z]<=1000 | chairPrice[z]<=2000 ){
             chairDiscount = 0.15;
-            printf("Price of chair #%d after discount is %f", chairPrice[z]*chairDiscount);
+            afterDiscount = chairPrice[z]-(chairPrice[z]*chairDiscount);
+            printf("Price of chair #%d after discount is %.3f\n", z+1, afterDiscount);
         }
 
         else if (chairPrice[z] > 2000){
             chairDiscount = 0.2;
-            printf("Price of chair #%d after discount is %f", chairPrice[z]*chairDiscount);
+            afterDiscount = chairPrice[z]-(chairPrice[z]*chairDiscount);
+            printf("Price of chair #%d after discount is %.3f\n", z+1, afterDiscount);
         }
         else{
            printf("Error");
         }
-        
 
         z++;
+        totalPrice = totalPrice+afterDiscount;
     }
-
-
-
+    printf("Total price after discount of all chairs which got a discount of 10 is %.3f\n", totalPrice);
 }
