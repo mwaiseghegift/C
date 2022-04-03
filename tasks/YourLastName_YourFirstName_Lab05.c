823c381c407dfbce4c29ddd02ab461f329cd3d91
retech <itsregalo047@gmail.com>
2022-04-03 22:59:28 +0300

    signals

---
+++ tasks/YourLastName_YourFirstName_Lab05.c
@@ -0,0 +1,29 @@
+/*
+CS 410 OS
+Lastname Firstname
+Lab 05
+*/
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(){
+
+    int i, n=10;
+    int firstTerm = 0;
+    int secondTerm = 1;
+    int nextTerm;
+
+    for (i = 0; i < n; i++){
+        printf("%d ", firstTerm);
+        sleep(1);
+        nextTerm = firstTerm + secondTerm;
+        firstTerm = secondTerm;
+        secondTerm = nextTerm;
+    }
+    printf("\n");
+    return 0;
+
+}
