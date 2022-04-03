4a20381b41b97925c336a1d9e0346a457008994f
retech <itsregalo047@gmail.com>
2022-04-04 00:51:44 +0300

    signals

--- tasks/YourLastName_YourFirstName_Lab05.c
+++ tasks/YourLastName_YourFirstName_Lab05.c
@@ -1,7 +1,8 @@
+
 /*
-CS 410 OS
-Lastname Firstname
-Lab 05
+    CS 410 OS
+    Lastname Firstname
+    Lab 05
 */
 
 
@@ -9,47 +10,45 @@ Lab 05
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-
-// set up a signal handler for SIGSTOP and SIGCONT
-void sig_handler(int signo) {
-    if (signo == SIGSTOP) {
-        printf("SIGSTOP\n");
-    } else if (signo == SIGCONT) {
-        printf("SIGCONT\n");
-    }
-}
+#include <sys/wait.h>
 
 int main(){
+    int pid = fork();
     int i, n=10;
-    int firstTerm = 0;
-    int secondTerm = 1;
-    int nextTerm;
+    int firstTerm = 0, secondTerm = 1, nextTerm;
+    int currentIndex;
 
-
-    for (i = 0; i < n; i++){
-        printf("%d ", firstTerm);
-        fflush(stdout);
-        sleep(1);
-        nextTerm = firstTerm + secondTerm;
-        firstTerm = secondTerm;
-        secondTerm = nextTerm;
-
-        // use SIGSTOP to pause the process at i == 4, and SIGCONT to resume
-        if (i == 4){
-            kill(getpid(), SIGSTOP);
-            printf("Paused\n");
-            fflush(stdout);
-            sleep(1);
-            kill(getpid(), SIGCONT);
-            printf("Resumed\n");
-            fflush(stdout);
-            sleep(1);
-        }
-
-       
+    if (pid == -1){
+        return 1;
     }
 
-    printf("\n");
-    return 0;
+    if (pid == 0){
+        // child
+        for (i = 0; i < n; i++){
+            printf("%d ", firstTerm);
+            fflush(stdout);
+            sleep(1);
+            nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
 
+            currentIndex = i;
+
+        }
+    }
+
+    else {
+        sleep(5);
+        // kill SIGSTOP with a message
+        printf("\nSending SIGSTOP to child\n");
+        kill(pid, SIGSTOP);
+
+        printf("\n");
+        sleep(5);
+        printf("Sending SIGCONT to child\n");
+        sleep(1);
+        kill(pid, SIGCONT);
+        sleep(5);
+        kill(pid, SIGSTOP);
+    }
 }
--- tasks/signals.c
+++ tasks/signals.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <signal.h>
-
+#include <sys/wait.h>
 
 int main(){
     int pid = fork();
     int i, n=10;
-    int firstTerm = 0;
-    int secondTerm = 1;
-    int nextTerm;
+    int firstTerm = 0, secondTerm = 1, nextTerm;
+    int currentIndex;
 
     if (pid == -1){
         return 1;
@@ -18,15 +17,30 @@ int main(){
     if (pid == 0){
         // child
         for (i = 0; i < n; i++){
-        printf("%d ", firstTerm);
-        fflush(stdout);
-        sleep(1);
-        nextTerm = firstTerm + secondTerm;
-        firstTerm = secondTerm;
-        secondTerm = nextTerm;
-    }}
+            printf("%d ", firstTerm);
+            fflush(stdout);
+            sleep(1);
+            nextTerm = firstTerm + secondTerm;
+            firstTerm = secondTerm;
+            secondTerm = nextTerm;
+
+            currentIndex = i;
+
+        }
+    }
 
     else {
-        wait(NULL);
+        sleep(5);
+        // kill SIGSTOP with a message
+        printf("\nSending SIGSTOP to child\n");
+        kill(pid, SIGSTOP);
+
+        printf("\n");
+        sleep(5);
+        printf("Sending SIGCONT to child\n");
+        sleep(1);
+        kill(pid, SIGCONT);
+        sleep(5);
+        kill(pid, SIGSTOP);
     }
 }
