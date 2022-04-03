de01470a73094fceaa84bc3d856d6535ce15300b
retech <itsregalo047@gmail.com>
2022-04-03 23:49:07 +0300

    signals

--- tasks/YourLastName_YourFirstName_Lab05.c
+++ tasks/YourLastName_YourFirstName_Lab05.c
@@ -10,11 +10,21 @@ Lab 05
 #include <unistd.h>
 #include <signal.h>
 
+// set up a signal handler for SIGSTOP and SIGCONT
+void sig_handler(int signo) {
+    if (signo == SIGSTOP) {
+        printf("SIGSTOP\n");
+    } else if (signo == SIGCONT) {
+        printf("SIGCONT\n");
+    }
+}
+
 int main(){
     int i, n=10;
     int firstTerm = 0;
     int secondTerm = 1;
     int nextTerm;
+
 
     for (i = 0; i < n; i++){
         printf("%d ", firstTerm);
@@ -24,12 +34,19 @@ int main(){
         firstTerm = secondTerm;
         secondTerm = nextTerm;
 
-        // A kill signal to terminate the program when i == 4 and resume after some time)
+        // use SIGSTOP to pause the process at i == 4, and SIGCONT to resume
         if (i == 4){
-            printf("Program terminated\n");
-            kill(getpid(), SIGKILL);
-
+            kill(getpid(), SIGSTOP);
+            printf("Paused\n");
+            fflush(stdout);
+            sleep(1);
+            kill(getpid(), SIGCONT);
+            printf("Resumed\n");
+            fflush(stdout);
+            sleep(1);
         }
+
+       
     }
 
     printf("\n");
