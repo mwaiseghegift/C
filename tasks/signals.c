109ed9b6aa7daf9ede4495fa5581330a9c744279
retech <itsregalo047@gmail.com>
2022-04-03 23:54:21 +0300

    signals

--- tasks/signals.c
+++ tasks/signals.c
@@ -6,6 +6,10 @@
 
 int main(){
     int pid = fork();
+    int i, n=10;
+    int firstTerm = 0;
+    int secondTerm = 1;
+    int nextTerm;
 
     if (pid == -1){
         return 1;
@@ -13,8 +17,14 @@ int main(){
 
     if (pid == 0){
         // child
-        
-    }
+        for (i = 0; i < n; i++){
+        printf("%d ", firstTerm);
+        fflush(stdout);
+        sleep(1);
+        nextTerm = firstTerm + secondTerm;
+        firstTerm = secondTerm;
+        secondTerm = nextTerm;
+    }}
 
     else {
         wait(NULL);
